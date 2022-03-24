59abc427756f6e564daf0c8216ca889bf4b7e29e
Santiago Torres Alvarez <55812138+Chewbacaguy@users.noreply.github.com>
2022-03-24 15:01:56 -0600

    Try de que jale la api

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
 import './style.css';
 
 async function fetchDataFromAPIEndPoint() {
-  const cards = await fetch('api/fetchNotion').then((res) =>
-    res.json().then((data) => data.results)
-  );
+  const cards = await fetch('/api/fetchNotion').then((res) =>
+    res.json().then((data) => data.results));
+
   document.querySelector('.card-container').innerHTML = cards
     .map(
       (card) => `
     <article class="card">
     <img
-      src="${card.properties.Image.files[0].external.url}"
+      src="${card.properties.img.files[0].external.url}"
       alt="${card.properties.Name.title[0].plain_text}"
       class="card__image"
     />
     <h2 class="card__heading">${card.properties.Name.title[0].plain_text}</h2>
     <div class="card__price">
-      <p>${card.properties.Price.rich_text[0].plain_text}</p>
-      <a href="${card.properties.Link.url}" class="card__btn">${card.properties.Btb_text.rich_text[0].plain_text}</a>
+      <p>${card.properties.Precio.rich_text[0].content}</p>
+      <a href="${card.properties.Link.url}" class="card__btn">${card.properties.btn_text.rich_text[0].plain_text}</a>
     </div>
   </article>
   `
